f318022d073b772df05f732750b97090d7a52f51
Kasper Ernest Alexander <ernestalexander.kasper@students.bfh.ch>
2021-12-09 23:21:16 +0100

    cant fing bug why video dosen't load

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div>
             <Navbar/>
             <Routes>
-                <Route path='/' exact component={Home}/>
+                <Route path='/' element={<Home/>}/>
             </Routes>
         </div>
     );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Button from "./Button";
+import '../App.css';
+import './HeroSection.css';
 
 function HeroSection(){
     return (
       <div className='hero-container'>
-          <video src="/public/videos/Madara.mp4" autoPlay loop muted />
+          <video autoPlay loop muted>
+              <source src="/public/Videos/video-1.mp4" type='video/mp4'/>
+                  <p>Sorry video is not supported</p>
+          </video>
           <hi>THE WAR BEGAN!</hi>
           <p>What are you waiting for?</p>
+          <div className="hero-btns">
+              <Button className='btns'
+                      buttonSize='btn--large'
+                      buttonSytle='btn--outline'>
+                  GET STARTED
+              </Button>
+              <Button className='btns'
+                      buttonSize='btn--large'
+                      buttonSytle='btn--outline'>
+                  WATCH TRAILER <i className='far far-play-circle'/>
+              </Button>
+          </div>
       </div>
     );
 }
